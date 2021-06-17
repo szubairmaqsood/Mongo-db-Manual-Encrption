214e5e6a689e0d9701c80b23d31bb630b3d1c3e9
zubair-pc\zubair <szubair@gmail.com>
2021-06-17 11:44:51 +0500

    Basic set up for project just spring web and also mnogo db driver for connection

---
+++ Mongo-DataBase-Encryption-Client-Side/src/main/kotlin/com/example/Mongo/DataBase/Encryption/Client/Side/MongoDataBaseEncryptionClientSideApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Mongo.DataBase.Encryption.Client.Side
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MongoDataBaseEncryptionClientSideApplication
+
+fun main(args: Array<String>) {
+	runApplication<MongoDataBaseEncryptionClientSideApplication>(*args)
+}
---
+++ Mongo-DataBase-Encryption-Client-Side/src/test/kotlin/com/example/Mongo/DataBase/Encryption/Client/Side/MongoDataBaseEncryptionClientSideApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Mongo.DataBase.Encryption.Client.Side
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongoDataBaseEncryptionClientSideApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
